532479529958a2fcdde7f0e04b1b328a9573d4a5
Dmitrii K <63146525+th3foxx@users.noreply.github.com>
2024-05-05 11:18:24 +0300

    improve

--- GuessNum/Game.cs
+++ GuessNum/Game.cs
@@ -1,29 +1,62 @@
-﻿namespace GuessNum;
+namespace GuessNum;
 
-public static class Game
+public class Game
 {
-    private static readonly Random Random = new Random();
-    public static void Run(int maxValue = 30)
+    private readonly Random _random = new Random();
+    private int _maxValue;
+
+    public Game(int maxValue = 30)
+    {
+        _maxValue = maxValue;
+    }
+
+    public virtual void Run()
     {
         int attempts = 3;
-        int guessedNumber = Random.Next(maxValue);
-        Console.WriteLine($"The computer guessed a number between 0 and {maxValue}");
+        int guessedNumber = _random.Next(_maxValue);
+        Console.WriteLine($"The computer guessed a number between 0 and {_maxValue}");
 
         while (attempts != 0) 
         {
-            int playerGuess = Player.MakeChoice(maxValue);
-            
-            if (playerGuess == guessedNumber)
-                break;
-            else if(playerGuess > guessedNumber) 
-                Console.WriteLine($"The number {playerGuess} is greater than what the computer guessed.");
-            else if(playerGuess < guessedNumber)
-                Console.WriteLine($"The number {playerGuess} is less than what the computer guessed.");
-            
-            attempts--;
+            if (int.TryParse(Player.MakeChoice(_maxValue, "Enter a number between 0 and {_maxValue}"), out int playerGuess))
+            {
+                if (playerGuess == guessedNumber)
+                {
+                    break;
+                }
+                else if (playerGuess > guessedNumber)
+                {
+                    Console.WriteLine($"The number {playerGuess} is greater than what the computer guessed.");
+                }
+                else if (playerGuess < guessedNumber)
+                {
+                    Console.WriteLine($"The number {playerGuess} is less than what the computer guessed.");
+                }
+
+                attempts--;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
         }
 
         string result = attempts != 0 ? "You've won!" : "You've lost!";
         Console.WriteLine($"{result}\nThe computer guessed the number {guessedNumber}");
+        Console.WriteLine("Do you want to play again? (y/n)");
+        if (Console.ReadLine().ToLower() == "y")
+        {
+            Run();
+        }
+    }
+}
+
+
+public static class Player
+{
+    public static string MakeChoice(int maxValue, string prompt = "Enter a number:")
+    {
+        Console.Write(prompt);
+        return Console.ReadLine();
     }
 }
